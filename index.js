ba46bbd8a0a1ff4729ffbbd3811d6ac709a5eb6e
alexzhaohong <alexhong96@gmail.com>
2020-04-25 11:54:26 -0700

    Add config TIMES route

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(showTimes()))
   .get('/cool', (req, res) => res.send(coolAsciiFaces()) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  let showTimes = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    return result;
+  }
