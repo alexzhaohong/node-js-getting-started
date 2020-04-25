e7e34818b19f800502b86d3bfcd2153c4de2e71e
alexzhaohong <alexhong96@gmail.com>
2020-04-25 11:54:26 -0700

    Add config TIMES route

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const path = require('path')
 const coolAsciiFaces = require('cool-ascii-faces')
+const {Pool} = require('pg')
 
 const PORT = process.env.PORT || 5000
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/times', (req, res) => res.send(showTimes()))
   .get('/cool', (req, res) => res.send(coolAsciiFaces()) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  let showTimes = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    return result;
+  }
