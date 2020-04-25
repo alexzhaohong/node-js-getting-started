b813ee0ae86882896d4617954f4ab1ea219765b7
alexzhaohong <alexhong96@gmail.com>
2020-04-25 01:00:20 -0700

    Add cool ascii faces

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const coolAsciiFaces = require('cool-ascii-faces')
+
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(coolAsciiFaces()) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
